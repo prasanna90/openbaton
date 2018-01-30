062fb991103c9ac6d5525bed0c4ed61cb25d7d98
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-01-30 16:04:26 +0000

    modified sample.py

--- prasu/Scripts/sample.py
+++ prasu/Scripts/sample.py
@@ -2,19 +2,17 @@ import os
 
 
 def sample():
-    print ("hello world")
     cwddir = os.getcwd()
-    print ("the current working directoryis:", cwddir)
     #path = r"C:\Users\GURC4657\Desktop\ppp"
     path = r"Sampletest"
     if os.path.exists(path):
-        print("the directory already exists")
+        print "the directory already exists"
     else:
         os.mkdir(path)
-        print ("the directory got created")
+        print "the directory got created"
     os.chdir(path)
     with open("vmtest.txt", 'w') as f:
-        f.write("hello world\n Welcome to VM test file")
-    print ("file got added")
+        f.write "hello world\n Welcome to VM test file"
+
 sample()
 
