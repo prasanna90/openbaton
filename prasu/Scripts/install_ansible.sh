a7b296b302d8b54ab62cf76154250f0e85f6bb9f
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-01-31 13:39:20 +0000

    changes

--- prasu/Scripts/install_ansible.sh
+++ prasu/Scripts/install_ansible.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo apt-add-repository ppa:ansible/ansible && sudo apt-get update && sudo apt-get install ansible
+sudo apt-add-repository ppa:ansible/ansible
+
+expect "Press [ENTER] to continue or ctrl-c to cancel adding it"
+send "yes"
+
+sudo apt-get update && sudo apt-get install ansible
