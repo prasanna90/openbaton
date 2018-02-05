54a7ff548d111ee3b1b70a58ac1958ce78847b4a
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-05 12:00:42 +0000

    Small change in file

--- prasu/Scripts/bugplaybook.py
+++ prasu/Scripts/bugplaybook.py
@@ -6,7 +6,7 @@ def main():
     os.system("mv bugzilla /etc/ansible")
     os.system("mv bugzilla_ans.yml /etc/ansible")
     os.chdir("/etc/ansible")
-    print os.system("ansible 172.19.77.162 -m ping -u root")
+    print os.system("ansible 172.19.77.161 -m ping -u root")
     print os. system("ansible-playbook bugzilla_ans.yml")
 main()
 
