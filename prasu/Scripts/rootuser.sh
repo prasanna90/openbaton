94b4320e5bee81a49c132c6f2291e87f5c15a130
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-05 14:53:22 +0000

    ree

--- prasu/Scripts/rootuser.sh
+++ prasu/Scripts/rootuser.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 sudo su
-sudo passwd root
-abc123
-abc123
+#sudo passwd root 
+pass=abc123
+echo $pass | sudo passwd root
+#abc123
+#abc123
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service ssh restart
-ssh -o StrictHostKeyChecking=no -l root 172.19.77.161
-abc123
+echo $pass | ssh -o StrictHostKeyChecking=no -l root 172.19.77.161
 
+
+
+
+
+#echo $efi | sudo -S firmwarepasswd -verify
 
