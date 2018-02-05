c68d02114d7ec349625666291119334420320454
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-05 14:25:25 +0000

    modified

--- prasu/Scripts/rootuser.sh
+++ prasu/Scripts/rootuser.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-
 sudo su
 sudo passwd root
-echo abc123
-echo abc123
+abc123
+abc123
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service ssh restart
+ssh -o StrictHostKeyChecking=no -l root 172.19.77.161
+abc123
+
 
