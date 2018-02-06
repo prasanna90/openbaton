ec4c4fcd48595afb27f38114ebc8be50d3532454
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-06 16:15:53 +0000

    asdsdsa

--- prasu/Scripts/rootuser.sh
+++ prasu/Scripts/rootuser.sh
@@ -8,7 +8,3 @@ expect "Retype new UNIX password:"
 send "abc123\r"
 interact
 
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo service ssh restart
-
