1d53b97c212f3d54791bd0fc2c776bef882f1af0
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-07 17:11:11 +0000

    asdsdfs

--- prasu/Scripts/install_ansible.sh
+++ prasu/Scripts/install_ansible.sh
@@ -4,7 +4,7 @@
 
 #sudo apt-get update 
 
-yes Y | sudo apt-get install ansible 
+sudo apt-get install ansible 
 sudo apt-get install expect
 sudo apt-get install sshpass
 sudo apt-get install python-pexpect
