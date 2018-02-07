3d1f0bcd7977a893f240a6d64b06709580ddc03b
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-07 09:38:06 +0000

    modified

--- prasu/Scripts/install_ansible.sh
+++ prasu/Scripts/install_ansible.sh
@@ -2,12 +2,12 @@
 
 #yes Y | sudo apt-add-repository ppa:ansible/ansible
 
-sudo apt-get update 
+#sudo apt-get update 
 
 yes Y | sudo apt-get install ansible 
 sudo apt-get install expect
 sudo apt-get install sshpass
 
 
-expect rootuser.sh
-expect ssh.sh
+#expect rootuser.sh
+#expect ssh.sh
