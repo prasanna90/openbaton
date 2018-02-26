d27f40f634f5af3cfb33f359d4293c40f86b3ee8
lakshmiprasanna <karnam.prasanna@aricent.com>
2018-02-26 11:19:29 +0000

    Added changes to the script

--- prasu/Scripts/install_ansible.sh
+++ prasu/Scripts/install_ansible.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#yes Y | sudo apt-add-repository ppa:ansible/ansible
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ansible
 sudo apt-get update 
+sudo apt-get upgrade
 
-#yes Y | sudo apt-get install ansible 
+yes Y | sudo apt-get install ansible 
 sudo apt-get install expect
 sudo apt-get install sshpass
 sudo apt-get install python-pexpect
